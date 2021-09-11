b9ca33e5dbf98f04db26acc6e422c6df1cb516c0
viveking786 <65109405+viveking786@users.noreply.github.com>
2021-09-11 12:11:01 +0530

    commit

--- add-patient.php
+++ add-patient.php
@@ -4,173 +4,72 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Pathology Management System</title>
-    <link rel="stylesheet" href="Vendor/Bootstrap/css/bootstrap.css">
+    <title>Patient</title>
     <link rel="stylesheet" href="Src/css/style.css">
-    <script src="Vendor/Jquery/jquery.js"></script>
-    <script src="Vendor/FontAwesome/all.js"></script>
-    <script src="Vendor/Bootstrap/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="Src/fonts/Poppins/poppins-fonts.css">
-    <style>
-        html,body{
-            height: 100%;
-        }
-    </style>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="Vendor/Bootstrap/css/bootstrap.css">
+    <!-- jQuery library -->
+    <script src="Vendor/Jquery/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="Vendor/Bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="Vendor/FontAwesome/fontawesome.js"></script>
+    <script src="Src/js/script.js"></script>
 </head>
-<body class="dashboard">
-    <div class="wrapper">
-        <nav class="nav flex-column flex-nowrap vh-100 overflow-auto text-white p-2" id="sidebar">
-            <div class="sidebar_header">
-                <h3>Dashboard</h3>
-                <hr>
-            </div>
-            <ul class="list-unstyled components">
-                <li><a href="dashboard.php">Home</a></li>
-                <li> <a href="#patientmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">Patient</a>
-                <ul class="collapse list-unstyled" id="patientmenu">
-                    <li> <a href="#" class="active">Add Patient</a></li>
-                    <li> <a href="#">Patinet's List</a></li>
-                    <li> <a href="#">Search Patient</a></li>
-                </ul>
-                </li>
-                <li><a href="#">Master Page</a></li>   
-                <li> <a href="#testmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">Test</a>
-                <ul class="collapse list-unstyled" id="testmenu">
-                    <li> <a href="#">Test Entry</a></li>
-                    <li> <a href="#">Test Update</a></li>
-                    <li> <a href="#">Manual Entry</a></li>
-                    <li> <a href="#">Manual Update</a></li>
-                    <li> <a href="#">Test Rate List</a></li>
-                </ul>
-                </li> 
-                <li><a href="#">Account Details</a></li>
-                <li><a href="#">Backup & Restore</a></li>    
-                <li><a href="#">Manage User</a></li>       
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Logout</a></li>   
-                                                                              
-            </ul>
-        </nav>
-        <div class="container-fluid page_area_cst">
-            <nav class="navbar navbar-expand-lg navbar-light nvbar_cstm"> <button type="button" id="sidebarCollapse" class="btn btn-info"> <i class="fas fa-bars"></i> </button> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto list-unstyled components"> 
-                        <li><a class="pr-3 full_scn_btn_fa" onclick="launchFullScreen(document.documentElement)"><i class="fas fa-expand-arrows-alt"></i></a></li>               
-                    <li> <a href="#adminarea" data-toggle="modal" aria-expanded="true" data-target="#exampleModal">ADMIN</a>
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                            <div class="modal-header modal_adm_bg">
-                                <h5 class="modal-title" id="exampleModalLabel">Admin</h5>
-                                <!-- <p class="modal_subTl">Laboratory Name</p> -->
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                            </div>
-                            </div>
-                        </div>
-                        </div>                        
-                    </li>   
-                </ul>                    
-                </div>
-            </nav>            
-            <nav class="text-right">
-                                 
-            </nav>
-            <div class="row">
-                <div class="col-sm-12">
-                    <h1 class="page_title">Add Patient</h1>
-                        <!-- <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Library</li>
-                            </ol>
-                        </nav> -->
-                        <div class="col-sm-12">
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 mx-auto">
                 <div class="register-form">
                     <h2 class="text-center head_tt_mn">Patient Registration</h2>
-                    <form action="#">
+                    <form action="#" method="post" class="has-validation" id="patientForm" >
                         <div class="form-group">
                           <label for="name" class="label_main">Patient Name:</label>
-                          <input type="text" class="form-control" placeholder="Patient name" id="name">
+                          <input type="text" class="form-control" placeholder="Patient name" id="name" name="name">
                         </div>
                         <div class="form-group">
                           <label for="age" class="label_main">Patient Age:</label>
-                          <input type="text" class="form-control" placeholder="Patient age" id="age">
+                          <input type="text" class="form-control" placeholder="Patient age" id="age" name="age">
                         </div>
                         <div class="form-group">
                             <label for="coll-date" class="label_main">Sample Collection Date:</label>
-                            <input type="datetime-local" class="form-control" placeholder="Collection date" id="coll-date">
+                            <input type="datetime-local" class="form-control" placeholder="Collection date" id="coll-date" name="coll-date">
                         </div>  
                         <label for="" class="label_main">Patient Gender:</label> 
                         <div class="form-group rd_bg_cst1">
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" class="custom-control-input" id="male" name="example" value="customEx">
+                                <input type="radio" class="custom-control-input" id="male" name="gender" value="Male">
                                 <label class="custom-control-label" for="male">Male</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" class="custom-control-input" id="female" name="example" value="customEx">
+                                <input type="radio" class="custom-control-input" id="female" name="gender" value="Female">
                                 <label class="custom-control-label" for="female">Female</label>
                             </div> 
                         </div> 
                         <label for="" class="label_main">Child:</label> 
                         <div class="form-group rd_bg_cst2">
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" class="custom-control-input" id="childyes" name="example" value="customEx">
+                                <input type="radio" class="custom-control-input" id="childyes" name="child" value="1">
                                 <label class="custom-control-label" for="childyes">Yes</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" class="custom-control-input" id="childno" name="example" value="customEx">
+                                <input type="radio" class="custom-control-input" id="childno" name="child" value="0">
                                 <label class="custom-control-label" for="childno">No</label>
                             </div> 
                         </div> 
                         <div class="form-group">
                             <label for="doc-ref" class="label_main">Referred By Doctor:</label>
-                            <input type="text" class="form-control" placeholder="Doctor name" id="doc-ref">
+                            <input type="text" class="form-control" placeholder="Doctor name" id="doc-ref" name="doc-ref">
                         </div>  
                         <div class="form-group">
                             <label for="smp-sent" class="label_main">Sample Sent:</label>
-                            <input type="text" class="form-control" placeholder="Self" id="smp-sent">
+                            <input type="text" class="form-control" placeholder="Self" id="smp-sent" name="smp-sent">
                         </div>     
-                        <button type="submit" class="btn btn-primary btn-block"><i class="fa">&#xf061;</i> Next</button>
+                        <button type="submit" class="btn btn-primary btn-block" id="submit"><i class="fa">&#xf061;</i> Next</button>
                       </form>
                 </div>
             </div>
-                    <div class="line"></div>                    
-                </div>
-            </div>            
         </div>
     </div>
-
-<script>
-    $(document).ready(function()
-    {
-        $("#sidebarCollapse").on('click', function()
-        {
-            $("#sidebar").toggleClass('active');
-        });
-    });    
-
-    // Launching full screen
-    function launchFullScreen(element){
-        if(element.requestFullScreen){
-            element.requestFullScreen();
-        }
-        else if(element.mozRequestFullScreen) {
-		    element.mozRequestFullScreen();
-	    } 
-	    else if(element.webkitRequestFullscreen) {
-		    element.webkitRequestFullscreen();
-	    } 
-	    else if(element.msRequestFullscreen) {
-		    element.msRequestFullscreen();
-	    }        
-    }
-</script>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,58 +1,74 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup - Pathology Management System</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="Vendor/Bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="Src/css/style.css">
     <script src="Vendor/Jquery/jquery.js"></script>
     <script src="Vendor/FontAwesome/all.js"></script>
-    <link rel="stylesheet" href="Src/fonts/Poppins/poppins-fonts.css">
-    <link rel="stylesheet" href="Src/fonts/Roboto/Roboto-stylesheet.css">
 </head>
-<body class="signup_body">
+<body class="indx_body">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 mx-auto signup_form_cst">
+            <div class="col-sm-5 mx-auto login_frm_cst">
                 <div class="logo_cstm text-center">
                     <img src="Src/img/patholgoy-logo.svg" alt="">
                 </div>
-                <form action="#" method="post" class="mt-5">
+                <form action="Controllers/SignupController.php" method="post" class="mt-5">
                     <div class="frm_head text-center">
-                        <h1 style="    font-family: 'Poppins-SemiBold'; font-weight: bold; font-style: normal;">SignUp</h1>
+                        <h1>Sign Up</h1>
                     </div>
                     <hr>
-                    <div class="row sgn_frm_inner">
-                        <div class="col-sm-12 sfi_pd">
-                            <label for="fname" class="mr-sm-2">Full Name</label>
-                            <input type="text" class="form-control" id="fname" placeholder="Enter Full Name" name="fullname">
+                    <div class="input-group mb-3 mt-4">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text cstm_icnns" id="basic-addon1"><label for="name"></label><i class="fa fa-user"></i></label></span>
                         </div>
-                        <div class="col-sm-12 mt-3 sfi_pd">
-                            <label for="email" class="mr-sm-2">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email">
-                        </div>
-                        <div class="col-sm-12 mt-3 sfi_pd">
-                            <label for="phone" class="mr-sm-2">Phone</label>
-                            <input type="text" class="form-control" id="phone" placeholder="Enter Phone Number" name="phone">
-                        </div>
-                        <div class="col-sm-12 mt-3 sfi_pd">
-                            <label for="password" class="mr-sm-2">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password">
-                        </div>
-                        <div class="col-sm-12 mt-3">
-                            <div class="form-check frm_cst_cbx">
-                                <input type="checkbox" class="form-check-input check_box_cstm" id="exampleCheck1" onclick="toggle()">
-                                <label class="form-check-label ssho_pas_labl" for="exampleCheck1">Show Password</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block btn_sbmt" name="sub">Next</button>
-                        </div>
+                        <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" id="name" name="name">
                     </div>
-                </form>
+
+                    <div class="input-group mb-3 mt-4">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text cstm_icnnstwo" id="basic-addon1"><label for="email"></label><i class="fa fa-envelope"></i></label></span>
+                        </div>
+                        <input type="email" class="form-control" placeholder="Email Address" aria-label="Email" aria-describedby="basic-addon1" id="email" name="email">
+                    </div>
+
+                    <div class="input-group mb-3 mt-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text cstm_icnnstwo" id="basic-addon1"><label for="phone"></label><i class="fa fa-phone"></i></label></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Phone Number" aria-label="Phone" aria-describedby="basic-addon1" id="phone" name="phone">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text cstm_icnnstwo" id="basic-addon1"><i class="fas fa-lock" style="font-size: 18px;"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" id="password" name="password">
+                        <!-- <i class="fa fa-eye" aria-hidden="true"></i> -->
+                    </div>
+                    <!-- <div class="form-group form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                    <hr>
+                    <div class="form-check frm_cst_cbx">
+						<input type="checkbox" class="form-check-input check_box_cstm" id="exampleCheck1" onclick="toggle()">
+						<label class="form-check-label ssho_pas_labl" for="exampleCheck1">Show Password</label>
+					</div>
+                    <button type="submit" class="btn btn-primary btn-block btn_sbmt" name="signup">Sign Up</button>
+                  </form>
             </div>
         </div>
     </div>
+
+
 
     <!-- Script -->
 
@@ -65,7 +81,19 @@
 			else{
 				temp.type = "password";
 			}
+			var c = document.getElementById('confirm');
+			if(c.type==='password')
+            {
+                c.type='text';
+            }
+            else
+            {
+                c.type='password';
+            }
+
 		}
-	</script>    
+	$(document).ready()
+
+	</script>
 </body>
 </html>
